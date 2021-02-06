457d8cc48bd41c4f250adf6cf89521826bc4371d
eugenerush <64245338+eugenerush@users.noreply.github.com>
2021-02-06 12:24:50 +0300

    Add heroku files

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ async def main():
     i = 15
     while i < 15:
         await client.send_message(-1001461743664, 'Test spam block chat')
-        await client.send_file(-1001461743664, 'C:/Users/Роман/Desktop/tg/photo.jpg')
+        await client.send_file(-1001461743664, 'C:/Users/Stex/Desktop/tg_bot/photo.jpg')
         time.sleep(2)
 
 
