bafe1f1f5e5880f7cc619cb4946aaf8d8007031a
eugenerush <64245338+eugenerush@users.noreply.github.com>
2021-02-06 12:35:40 +0300

    Rename files

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-from telethon.sync import TelegramClient, events
-import time
+import telebot
+from telebot import types
 
-api_id = 1200079
-api_hash = 'e278d876a0cfdca2dc7705e1ec2f9d55'
-client = TelegramClient('anon', api_id, api_hash)
+bot = telebot.TeleBot("1573024390:AAE_pgyDydpY2OpE1b8Uc0OzQy0S05xRb5c")
 
 
-async def main():
-    i = 15
-    while i < 15:
-        await client.send_message(-1001461743664, 'Test spam block chat')
-        await client.send_file(-1001461743664, 'C:/Users/Stex/Desktop/tg_bot/photo.jpg')
-        time.sleep(2)
+@bot.message_handler(commands=['start'])
+def start(message):
+    bt = types.InlineKeyboardMarkup()
+    send = types.InlineKeyboardButton(text='Начать рассылку', callback_data='dog')
+    bt.add(send)
+    bot.send_message(message.chat.id, 'Нажми на кнопку и рассылка начнется через пару секунд', reply_markup=bt)
 
 
-with client:
-    client.loop.run_until_complete(main())
+bot.polling()
---
+++ spam-part.py
@@ -0,0 +1,18 @@
+from telethon.sync import TelegramClient, events
+import time
+
+api_id = 1200079
+api_hash = 'e278d876a0cfdca2dc7705e1ec2f9d55'
+client = TelegramClient('anon', api_id, api_hash)
+
+
+async def main():
+    i = 15
+    while i < 15:
+        await client.send_message(-1001461743664, 'Test spam block chat')
+        await client.send_file(-1001461743664, 'C:/Users/Stex/Desktop/tg_bot/photo.jpg')
+        time.sleep(2)
+
+
+with client:
+    client.loop.run_until_complete(main())
