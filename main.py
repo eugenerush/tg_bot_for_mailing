c799f783c8732504ebbb6da7ff20e3960e54d1ec
eugenerush <64245338+eugenerush@users.noreply.github.com>
2021-02-14 18:15:30 +0300

    release1.0

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telethon.sync import TelegramClient
-from telethon.tl.functions.channels import JoinChannelRequest
 import time
+import random
+from list import chat_id
 
 api_id = 3636832
 api_hash = '775dbb2c1edce433a4dac365f78a7faa'
@@ -9,25 +10,23 @@ t = open('text.txt', 'r', encoding='utf-8')
 text = t.read()
 
 
-'''async def main():
-    f = open('chats.txt', 'r')
-    i = 0
-    while i <= 155:
-        channel = f.readline()
-        await client(JoinChannelRequest(channel))
-        i += 1
-        time.sleep(5)'''
-
-
 async def spam():
-    #  s = open('chats.txt', 'r')
+    s = open('chats.txt', 'r')
+    n = 0
     while True:
         f = open('output.txt', 'r')
         i = f.readline()
-        #  channel = s.readline()
         if i == 'start_text_send':
-            await client.send_message('maykovskiy', text)
-            time.sleep(3)
+            await client.send_message(chat_id[n], text)
+            n += 1
+            time.sleep(random.randrange(3, 5, 1))
+            await client.send_message('maykovskiy', 'send ' + str(n) + ' from 122')
+            time.sleep(random.randrange(3, 8, 1))
+            if chat_id[n] == -1001459706290:
+                await client.send_message('maykovskiy', 'stop sleep wait')
+                time.sleep(13650)
+            else:
+                continue
         else:
             continue
 
